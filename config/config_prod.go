76d499414882a0b651caf37072cc89b429caf9f8
le.trong <le.trong>
2021-06-10 22:27:07 -0400

    Fixed mongodb config

--- config/config_prod.go
+++ config/config_prod.go
@@ -5,8 +5,8 @@ package config
 import "os"
 
 var host = os.Getenv("MONGO_DB_HOST")
-var username = os.Getenv("MONGO_DB_PASSWORD")
-var password = os.Getenv("MONGO_DB_USERNAME")
+var username = os.Getenv("MONGO_DB_USERNAME")
+var password = os.Getenv("MONGO_DB_PASSWORD")
 
 // AppConf constants
 var AppConf = AppConfig{
